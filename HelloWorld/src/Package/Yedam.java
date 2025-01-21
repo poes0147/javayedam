944831b06161cbb2ac826e6aafa43762b981c70f
admin <admin@DESKTOP-B5HH5I1>
2025-01-21 17:52:18 +0900

    은행만들기 숙제

---
+++ HelloWorld/src/Package/Yedam.java
@@ -0,0 +1,17 @@
+package Package;
+
+public class Yedam {
+	public static void main(String[] args) {
+		for (int i = 1 ; i <= 9 ; i++) {
+		for (int j = 2; j <= 9; j++) {
+			System.out.printf("%d * %d = %2d | ",  j , i , j*i);
+
+		  }
+		System.out.println();
+		}
+		System.out.println("end of prog");
+	}
+
+}
+// 2*1=2 3*1=3 
+// 2*2=4 3*2=6
